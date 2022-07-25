1fbb2953d5cb9e8dc528f5b11e0d5f8db304ac2c
Djimo van Berlo <djimovanberlo@gmail.com>
2022-07-25 12:02:45 +0200

    fixed publicruntime imports

--- templates/javascript/services/contentful/api/client.js
+++ templates/javascript/services/contentful/api/client.js
@@ -1,9 +1,9 @@
 import { GraphQLClient } from "graphql-request";
-import getConfig from "next/config";
 
-const { publicRuntimeConfig } = getConfig();
-const { CTF_CDA_ACCESS_TOKEN, CTF_CPA_ACCESS_TOKEN, CTF_SPACE_ID, CTF_ENV } =
-  publicRuntimeConfig;
+const CTF_CDA_ACCESS_TOKEN = process.env.NEXT_PUBLIC_CDA_ACCESS_TOKEN;
+const CTF_CPA_ACCESS_TOKEN = process.env.NEXT_PUBLIC_CPA_ACCESS_TOKEN;
+const CTF_ENV = process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT;
+const CTF_SPACE_ID = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
 
 const CONTENTFUL_URL = `https://graphql.contentful.com/content/v1/spaces/${CTF_SPACE_ID}`;
 
--- templates/typescript/config/next.config.js
+++ templates/typescript/config/next.config.js
@@ -1,36 +1,23 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path')
+const webpack = require('webpack')
 
 const envFilePath =
-  process.env.NODE_ENV === "development" ? "./.env.local" : "./.env";
+  process.env.NODE_ENV === 'development' ? './.env.local' : './.env'
 
-const { parsed: localEnv } = require("dotenv").config({
+const { parsed: localEnv } = require('dotenv').config({
   path: envFilePath,
-});
-
-const {
-  NEXT_PUBLIC_CDA_ACCESS_TOKEN,
-  NEXT_PUBLIC_CPA_ACCESS_TOKEN,
-  NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT,
-  NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-} = process.env;
+})
 
 module.exports = {
   reactStrictMode: true,
-  publicRuntimeConfig: {
-    CTF_CDA_ACCESS_TOKEN: NEXT_PUBLIC_CDA_ACCESS_TOKEN,
-    CTF_CPA_ACCESS_TOKEN: NEXT_PUBLIC_CPA_ACCESS_TOKEN,
-    CTF_ENV: NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT,
-    CTF_SPACE_ID: NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  },
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    domains: ["images.ctfassets.net"],
+    domains: ['images.ctfassets.net'],
   },
   webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv || {}));
-    return config;
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv || {}))
+    return config
   },
-};
+}
