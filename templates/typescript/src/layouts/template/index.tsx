a512441770dbda1dffdd3762226cf21e4d149118
Djimo van Berlo <djimovanberlo@gmail.com>
2022-11-01 15:37:35 +0100

    rename

--- templates/typescript/src/layouts/template/index.tsx
+++ templates/typescript/src/layouts/template/index.tsx
@@ -1,3 +1,3 @@
 export default function Template({ children }: { children: React.ReactNode }) {
-  return <div className="template">{children}</div>;
+  return <div className="template">{children}</div>
 }
--- templates/typescript/src/pages/_app.tsx
+++ templates/typescript/src/pages/_app.tsx
@@ -1,10 +1,15 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
 
-import "../styles/globals.css";
-import "../styles/main.scss";
+import Template from '@/layouts/template'
+import '../styles/globals.css'
+import '../styles/main.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Template>
+      <Component {...pageProps} />
+    </Template>
+  )
 }
 
-export default MyApp;
+export default MyApp
