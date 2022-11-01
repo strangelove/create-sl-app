d60f56b7a84dff16b9609279dacc9e25806716a9
Djimo van Berlo <djimovanberlo@gmail.com>
2022-11-01 15:29:01 +0100

    update layout

--- templates/typescript/src/layouts/section/section.tsx
+++ templates/typescript/src/layouts/section/section.tsx
@@ -1,3 +1,3 @@
 export default function Section({ children }: { children: React.ReactNode }) {
-  return <div className="section">{children}</div>;
+  return <div className="section">{children}</div>
 }
