3428f86e805c13dd1a0316699abc99c3b27ff80f
Djimo van Berlo <djimovanberlo@gmail.com>
2022-11-03 15:37:27 +0100

    typo updates

--- templates/typescript/src/components/typography/index.tsx
+++ templates/typescript/src/components/typography/index.tsx
@@ -1,36 +1,97 @@
 interface TypographyProps {
   children: React.ReactNode | string
   className?: string
+  id?: string
 }
 
-export const H1 = ({ children, className = '' }: TypographyProps) => (
-  <h1 className={`H1 ${className}`}>{children}</h1>
+export const H1s = ({ children, className = '', id = '' }: TypographyProps) => (
+  <h1 id={id} className={`H1s ${className}`}>
+    {children}
+  </h1>
 )
 
-export const H2 = ({ children, className = '' }: TypographyProps) => (
-  <h2 className={`H2 ${className}`}>{children}</h2>
+export const H1 = ({ children, className = '', id = '' }: TypographyProps) => (
+  <h1 id={id} className={`H1 ${className}`}>
+    {children}
+  </h1>
 )
 
-export const H3 = ({ children, className = '' }: TypographyProps) => (
-  <h3 className={`H3 ${className}`}>{children}</h3>
+export const H2 = ({ children, className = '', id = '' }: TypographyProps) => (
+  <h2 id={id} className={`H2 ${className}`}>
+    {children}
+  </h2>
 )
 
-export const H4 = ({ children, className = '' }: TypographyProps) => (
-  <h4 className={`H4 ${className}`}>{children}</h4>
+export const H3 = ({ children, className = '', id = '' }: TypographyProps) => (
+  <h3 id={id} className={`H3 ${className}`}>
+    {children}
+  </h3>
 )
 
-export const P1 = ({ children, className = '' }: TypographyProps) => (
-  <p className={`P1 ${className}`}>{children}</p>
+export const H4 = ({ children, className = '', id = '' }: TypographyProps) => (
+  <h4 id={id} className={`H4 ${className}`}>
+    {children}
+  </h4>
 )
 
-export const P2 = ({ children, className = '' }: TypographyProps) => (
-  <p className={`P2${className}`}>{children}</p>
+export const H5 = ({ children, className = '', id = '' }: TypographyProps) => (
+  <h5 id={id} className={`H5 ${className}`}>
+    {children}
+  </h5>
 )
 
-export const Bold = ({ children, className = '' }: TypographyProps) => (
-  <b className={`${className}`}>{children}</b>
+export const H6 = ({ children, className = '', id = '' }: TypographyProps) => (
+  <h6 id={id} className={`H6 ${className}`}>
+    {children}
+  </h6>
 )
 
-export const Italic = ({ children, className = '' }: TypographyProps) => (
-  <i className={`${className}`}>{children}</i>
+export const Q1 = ({ children, className = '', id = '' }: TypographyProps) => (
+  <q id={id} className={`Q1 ${className}`}>
+    {children}
+  </q>
+)
+
+export const Q2 = ({ children, className = '', id = '' }: TypographyProps) => (
+  <q id={id} className={`Q2 ${className}`}>
+    {children}
+  </q>
+)
+
+export const P1 = ({ children, className = '', id = '' }: TypographyProps) => (
+  <p id={id} className={`P1 ${className}`}>
+    {children}
+  </p>
+)
+
+export const P2 = ({ children, className = '', id = '' }: TypographyProps) => (
+  <p id={id} className={`P2 ${className}`}>
+    {children}
+  </p>
+)
+
+export const P3 = ({ children, className = '', id = '' }: TypographyProps) => (
+  <p id={id} className={`P3 ${className}`}>
+    {children}
+  </p>
+)
+
+export const Bold = ({
+  children,
+  className = '',
+  id = '',
+}: TypographyProps) => (
+  <b id={id} className={`${className}`}>
+    {children}
+  </b>
+)
+
+export const Italic = ({
+  children,
+  className = '',
+  id = '',
+}: TypographyProps) => (
+  <i id={id} className={`${className}`}>
+    {children}
+  </i>
 )
